97aaf0b9ee1c78dc1579ce7a50a167ffb0dc8476
duhongjun <hongjun.du.cn@gmail.com>
2022-02-25 10:44:17 +0800

    more examples

---
+++ error-handling/option.rs
@@ -0,0 +1,53 @@
+
+use std::option::Option;
+
+fn next_birthday(age : Option<u8>) -> Option<String> {
+    let mut int_age = age?;
+    int_age += 1;
+    Some(format!("next age: {}", int_age))
+}
+
+struct Person {
+    job: Option<Job>,
+}
+
+#[derive(Clone, Copy)]
+struct Job {
+    phone_number: Option<PhoneNumber>,
+}
+
+#[derive(Clone, Copy)]
+struct PhoneNumber {
+    area_code: Option<u8>,
+    number: u32,
+}
+
+impl Person {
+
+    // 获取此人的工作电话号码的区号（如果存在的话）。
+    fn work_phone_area_code(&self) -> Option<u8> {
+        // 没有`？`运算符的话，这将需要很多的嵌套的 `match` 语句。
+        // 这将需要更多代码——尝试自己编写一下，看看哪个更容易。
+        self.job?.phone_number?.area_code
+    }
+}
+
+fn main() {
+    let age = next_birthday(Some(8u8));
+    match age {
+        Some(s) => println!("{}",s),
+        None => println!("none"),
+    }
+
+    let p = Person {
+        job: Some(Job {
+            phone_number: Some(PhoneNumber {
+                area_code: Some(61),
+                number: 439222222,
+            }),
+        }),
+    };
+
+    assert_eq!(p.work_phone_area_code(), Some(61));
+
+}
