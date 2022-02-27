a28ad46e421d479236c566cc6418ca16819eb2c5
duhongjun <hongjun.du.cn@gmail.com>
2022-02-27 18:34:40 +0800

    more examples

--- macro/dry.rs
+++ macro/dry.rs
@@ -55,3 +55,4 @@ mod test {
     test!(sub_assign, 3u32, 2u32, 1u32);
 }
 
+
--- std-libs/arc.rs
+++ std-libs/arc.rs
@@ -2,6 +2,9 @@ use std::sync::Arc;
 use std::thread;
 
 fn main() {
+
+    let mut children = vec![];
+
     // 这个变量声明用来指定其值的地方。
     let apple = Arc::new("the same apple");
 
@@ -9,10 +12,15 @@ fn main() {
         // 这里没有数值说明，因为它是一个指向内存堆中引用的指针。
         let apple = Arc::clone(&apple);
 
-        thread::spawn(move || {
+        children.push(thread::spawn(move || {
             // 由于使用了Arc，线程可以使用分配在 `Arc` 变量指针位置的值来生成。
             println!("{:?}", apple);
-        });
+        }));
+    }
+
+    for child in children {
+        // 等待线程结束。返回一个结果。
+        let _ = child.join();
     }
 }
 
